7390c8d84235f7f76d906894d196e7592d6be2e8
Denzell Willis-Mackay <58393180+DFWTotal@users.noreply.github.com>
2020-01-23 06:44:24 -0500

    removed some code

---
+++ Hello_World.py
@@ -0,0 +1,24 @@
+
+for i in range(1, 100):
+    if i % 3 == 0 and i % 5 == 0:
+        print('fizzbuzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
+
+'''
+
+for num in range(1,101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
+'''
